677107861444568c8963fbc7c6d241a2136b253a
Pratik Jadhav <pratikjadhav9276@gmail.com>
2022-08-19 16:04:26 +0530

    Added new New Feature

--- src/Calculator.java
+++ src/Calculator.java
@@ -11,11 +11,21 @@ public class Calculator {
 		return num1*num2;
 	}
 	
+	public static int substraction(int num1,int num2)
+	{
+		return num1-num2;
+	}
+	public static int module(int num1,int num2)
+	{
+		return num1%num2;
+	}
 	public static void main(String arg[])
 	{
 		int num1,num2;
 		num1=10;num2=30;
-		System.out.println("Addition Of Two Number : "+Calculator.addition(num1, num2));
-		System.out.println("Multiplication Of Two Number : "+Calculator.multiplication(num1, num2));
+		System.out.println("Addition : "+Calculator.addition(num1, num2));
+		System.out.println("Multiplication : "+Calculator.multiplication(num1, num2));
+		System.out.println("Substraction : "+Calculator.multiplication(num1, num2));
+		System.out.println("Num1 % Num2 = "+Calculator.module(num1, num2));
 	}
 }
